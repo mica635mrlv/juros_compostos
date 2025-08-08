9b916712b3569f850d52c83cbf4dabcb7d373356
mica635mrlv <millaregina635@gmail.com>
2025-08-08 11:22:04 -0300

    adicionadas questões para o cliente e validação

--- app.js
+++ app.js
@@ -8,6 +8,10 @@
 // Importe feito para a biblioeca do Node
 var readline = require('readline');
 
+// Tratativa de erro
+const MESSAGE_ERRO_EMPTY = "ERRO: Não podem existir campos sem preenchimento";
+const MESSAGE_ERRO_INVALID = "VALOR INVÁLIDO: Insira apenas números positivos";
+
 // Tratamento dos dados inseridos pelo cliente
 var entradaDeDados = readline.createInterface({
     input:process.stdin,
@@ -15,8 +19,37 @@ var entradaDeDados = readline.createInterface({
 });
 
 // Perguntas feitas para o cliente
-console.log('**********Seja Bem-vinda à empresa Viva Moda**********')
+console.log('**********Seja Bem-vinda à empresa Viva Moda!**********')
+
+// Entrada de dado: nome do cliente
 entradaDeDados.question('Insira o seu nome: ', function(nome) {
     let nomeCliente = String(nome)
+
+    if (nomeCliente == '') { //Validando o nome
+        console.log(MESSAGE_ERRO_EMPTY);
+        entradaDeDados.close();
+    } else {
+
+         // Entrada de dado: nome do produto
+         entradaDeDados.question('Insira o nome do produto: ', function(produto) {
+            let nomeProduto = produto;
+
+            if (nomeProduto == '') { //Validando o produto
+                console.log(MESSAGE_ERRO_EMPTY);
+                entradaDeDados.close();
+            } else {
+
+                // Entrada de dados: capital inicial
+                entradaDeDados.question('Insira o valor do capital inicial: ', function(capital) {
+                    let capitalInicial = Number(capital);
+
+                    if (capital == '' || capitalInicial < 0 || isNaN(capitalInicial)) { //Validando o valor do capital
+                        console.log(MESSAGE_ERRO_INVALID);
+                        entradaDeDados.close;
+                    }
+                });
+            }
+        });
+    }
 });
 
